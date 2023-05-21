e25047e42ef34f1915ea6f495654696a893c4f0e
“yuki” <yukiksaay@gmail.com>
2023-05-21 19:05:41 +0900

    コンポーネント使用練習

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import Hello from './components/Hello';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-           これからReactの勉強を開始します。
-        </p>
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Hello />
   );
 }
 
---
+++ src/components/Hello.jsx
@@ -0,0 +1,11 @@
+const Hello = (props) =>{
+    return(
+        <div>
+            <h2>Hello React</h2>
+            <p>this is my first time to use component</p>
+        </div>
+    )
+};
+
+
+export default Hello;
